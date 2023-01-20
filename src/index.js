86a69de89a248eb90325bf8fa2ca00a7d3c77904
vbloodi <bloodi.code@gmail.com>
2023-01-20 18:32:58 +0300

    feat: complete towel-sort task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    let sortedMatrix = [];
+    if (!Array.isArray(matrix)) return [];
+    for (i = 0; i < matrix.length; ++i) {
+        if (i % 2 === 0) {
+            sortedMatrix = sortedMatrix.concat(matrix[i]);
+        } else {
+            sortedMatrix = sortedMatrix.concat(matrix[i].reverse());
+        }
+    }
+    return sortedMatrix;
+};
